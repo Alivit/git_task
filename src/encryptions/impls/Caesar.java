8162ca5cb52a97ab2061aabc3f376c1164e97c89
Alivit <vltal241201@gmail.com>
2023-02-05 23:21:38 +0300

    feat: add rus alphabet in Caesar

--- src/encryptions/impls/Caesar.java
+++ src/encryptions/impls/Caesar.java
@@ -15,10 +15,25 @@ public class Caesar implements Encryption {
         System.out.println("Введите новый ключ:");
         offset = scanner.nextInt();
         for (char character : message.toCharArray()) {
-            if (character != ' ') {
+            if(character >= 'a' && character <= 'z'){
                 int position = character - 'a';
                 int newPosition = (position + offset) % 26;
                 char newCharacter = (char) ('a' + newPosition);
+                result.append(newCharacter);
+            } else if(character >= 'A' && character <= 'Z') {
+                int position = character - 'A';
+                int newPosition = (position + offset) % 32;
+                char newCharacter = (char) ('A' + newPosition);
+                result.append(newCharacter);
+            } else if(character >= 'а' && character <= 'я') {
+                int position = character - 'а';
+                int newPosition = (position + offset) % 32;
+                char newCharacter = (char) ('а' + newPosition);
+                result.append(newCharacter);
+            } else if(character >= 'А' && character <= 'Я') {
+                int position = character - 'А';
+                int newPosition = (position + offset) % 32;
+                char newCharacter = (char) ('А' + newPosition);
                 result.append(newCharacter);
             } else {
                 result.append(character);
@@ -31,13 +46,37 @@ public class Caesar implements Encryption {
     public StringBuilder decoder(String message) {
         StringBuilder result = new StringBuilder();
         for (char character : message.toCharArray()) {
-            if (character != ' ') {
+            if (character >= 'a' && character <= 'z') {
                 int position = character - 'a';
                 int newPosition = ((position - offset) % 26);
                 if (newPosition < 0) {
                     newPosition = 26 + newPosition;
                 }
                 char newCharacter = (char) ('a' + newPosition);
+                result.append(newCharacter);
+            }else if (character >= 'A' && character <= 'Z') {
+                int position = character - 'A';
+                int newPosition = ((position - offset) % 26);
+                if (newPosition < 0) {
+                    newPosition = 26 + newPosition;
+                }
+                char newCharacter = (char) ('A' + newPosition);
+                result.append(newCharacter);
+            } else if (character >= 'а' && character <= 'я') {
+                int position = character - 'а';
+                int newPosition = ((position - offset) % 32);
+                if (newPosition < 0) {
+                    newPosition = 32 + newPosition;
+                }
+                char newCharacter = (char) ('а' + newPosition);
+                result.append(newCharacter);
+            } else if (character >= 'А' && character <= 'Я') {
+                int position = character - 'А';
+                int newPosition = ((position - offset) % 32);
+                if (newPosition < 0) {
+                    newPosition = 32 + newPosition;
+                }
+                char newCharacter = (char) ('А' + newPosition);
                 result.append(newCharacter);
             } else {
                 result.append(character);
